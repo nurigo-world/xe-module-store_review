408eeafb1a85ea7b1689c181fc2ca1ad3168abde
amy <amy@nurigo.net>
2015-06-23 16:19:52 +0900

    2nd test commit

--- store_review.class.php
+++ store_review.class.php
@@ -4,6 +4,7 @@
  * @author NURIGO(contact@nurigo.net)
  * @brief store_review module's high class
  * test modification
+ * 2nd test
  **/
 require_once(_XE_PATH_.'modules/store_review/store_review.item.php');
 class store_review extends ModuleObject {
